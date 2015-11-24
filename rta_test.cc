9fbdc4b16c217c07c9066d452383d9805b588afa
Luca Abeni <luca.abeni@unitn.it>
2015-11-24 22:49:12 +0100

    Better RTA example

--- rta_test.cc
+++ rta_test.cc
@@ -8,7 +8,8 @@ typedef task<2, 4, 4> t1;
 typedef task<3, 15, 15> t2;
 typedef task<5, 28, 28> t3;
 
-typedef struct TaskSet<t3, struct TaskSet<t2, struct TaskSet<t1, struct EmptyTS> > > ts;
+typedef struct TaskSet<t2, struct TaskSet<t1, struct TaskSet<t3, struct EmptyTS> > > ts;
+//typedef struct TaskSet<t3, struct TaskSet<t2, struct TaskSet<t1, struct EmptyTS> > > ts;
 
 int main()
 {
